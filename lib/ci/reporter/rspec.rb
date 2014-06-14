c05abe237dfc2558d9d37ed67eeffed03e4929f0
Gary Rennie <webmaster@gazler.com>
2014-06-14 17:21:06 +0100

    Feature: RSpec 3 support
    The RSpec 3 formatter currently replaces the RSpec 2 formatter.
    
    The class is called RSpecFormatter and aliased to RSpec because the
    following error is raised when naming the class RSpecFormatter directly.
    
        rspec.rb:53:in `<module:Reporter>': superclass mismatch for class RSpec (TypeError)
    
    RSpec 3 has completely overhauled the formatter system using a
    notification system that allows developers to optionally register
    interest in particular notifications instead of implementing them all.
    
    Please read:
    
      http://myronmars.to/n/dev-blog/2013/07/the-plan-for-rspec-3 for more information.

--- lib/ci/reporter/rspec.rb
+++ lib/ci/reporter/rspec.rb
@@ -2,22 +2,9 @@ require 'ci/reporter/core'
 
 module CI
   module Reporter
-    module RSpecFormatters
-      require 'rspec/core/formatters/base_formatter'
-      require 'rspec/core/formatters/progress_formatter'
-      require 'rspec/core/formatters/documentation_formatter'
-      BaseFormatter = ::RSpec::Core::Formatters::BaseFormatter
-      ProgressFormatter = ::RSpec::Core::Formatters::ProgressFormatter
-      DocFormatter = ::RSpec::Core::Formatters::DocumentationFormatter
-      # See https://github.com/nicksieger/ci_reporter/issues/76 and
-      #     https://github.com/nicksieger/ci_reporter/issues/80
-      require 'rspec/core/version'
-      RSpec_2_12_0_bug = (::RSpec::Core::Version::STRING == '2.12.0' &&
-                          !BaseFormatter.instance_methods(false).map(&:to_s).include?("format_backtrace"))
-    end
 
     # Wrapper around a <code>RSpec</code> error or failure to be used by the test suite to interpret results.
-    class RSpec2Failure
+    class RSpecFailure
       attr_reader :exception
 
       def initialize(example, formatter)
@@ -51,9 +38,10 @@ module CI
         output.push "#{exception.class.name << ":"}" unless exception.class.name =~ /RSpec/
         output.push @exception.message
 
-        format_metadata = RSpecFormatters::RSpec_2_12_0_bug ? @example.metadata : @example
+        backtrace_formatter = ::RSpec::Core::BacktraceFormatter.new
+        backtrace = backtrace_formatter.format_backtrace(@exception.backtrace)
 
-        [@formatter.format_backtrace(@exception.backtrace, format_metadata)].flatten.each do |backtrace_info|
+        backtrace.each do |backtrace_info|
           backtrace_info.lines.each do |line|
             output.push "     #{line}"
           end
@@ -62,71 +50,63 @@ module CI
       end
     end
 
-    # Custom +RSpec+ formatter used to hook into the spec runs and capture results.
-    class RSpec
-      attr_accessor :report_manager
-      attr_accessor :formatter
-      def initialize(*args)
-        @formatter ||= RSpecFormatters::ProgressFormatter.new(*args)
+    class RSpecFormatter < ::RSpec::Core::Formatters::ProgressFormatter
+      attr_accessor :suite, :report_manager, :output
+      if ::RSpec::Core::Formatters.respond_to?(:register)
+       ::RSpec::Core::Formatters.register self, :example_group_started,
+                                         :example_started, :example_passed, :example_failed,
+                                         :example_pending, :dump_summary
+     end
+
+      def initialize(output)
+        @output = output
         @report_manager = ReportManager.new("spec")
-        @suite = nil
       end
 
-      def example_group_started(example_group)
-        @formatter.example_group_started(example_group)
-        new_suite(description_for(example_group))
+      def example_group_started(notification)
+        new_suite(description_for(notification.group))
       end
 
-      def example_started(name_or_example)
-        @formatter.example_started(name_or_example)
+      def example_started(notification)
         spec = TestCase.new
         @suite.testcases << spec
         spec.start
       end
 
-      def example_failed(name_or_example, *rest)
-        @formatter.example_failed(name_or_example, *rest)
+      def example_passed(notification)
+        spec = @suite.testcases.last
+        spec.finish
+        spec.name = description_for(notification.example)
+      end
 
+
+      def example_failed(notification, *rest)
+        output.puts notification.example.execution_result
+        #
         # In case we fail in before(:all)
-        example_started(name_or_example) if @suite.testcases.empty?
-
-        failure = RSpec2Failure.new(name_or_example, @formatter)
+        example_started(notification) if @suite.testcases.empty?
+        failure = RSpecFailure.new(notification.example, self)
 
         spec = @suite.testcases.last
         spec.finish
-        spec.name = description_for(name_or_example)
+        spec.name = description_for(notification.example)
         spec.failures << failure
       end
 
-      def example_passed(name_or_example)
-        @formatter.example_passed(name_or_example)
-        spec = @suite.testcases.last
-        spec.finish
-        spec.name = description_for(name_or_example)
-      end
 
-      def example_pending(*args)
-        @formatter.example_pending(*args)
-        name = description_for(args[0])
-        spec = @suite.testcases.last
-        spec.finish
-        spec.name = "#{name} (PENDING)"
-        spec.skipped = true
-      end
-
-      def dump_summary(*args)
-        @formatter.dump_summary(*args)
+      def dump_summary(summary)
         write_report
-        @formatter.dump_failures
       end
 
-      def respond_to?(*args)
-        @formatter.respond_to?(*args)
+      def write_report
+        suite.finish
+        report_manager.write_report(suite)
       end
 
-      # Pass through other methods to RSpec formatter for compatibility
-      def method_missing(meth,*args,&block)
-        @formatter.send(meth,*args,&block)
+      def new_suite(name)
+        write_report if @suite
+        @suite = TestSuite.new name
+        @suite.start
       end
 
       private
@@ -141,20 +121,9 @@ module CI
           "UNKNOWN"
         end
       end
-
-      def write_report
-        if @suite
-          @suite.finish
-          @report_manager.write_report(@suite)
-        end
-      end
-
-      def new_suite(name)
-        write_report if @suite
-        @suite = TestSuite.new name
-        @suite.start
-      end
     end
+
+    RSpec = RSpecFormatter
 
     class RSpecDoc < RSpec
       def initialize(*args)
